7bbaaca898ef1bbfae091714484b53083cbabb81
ClementPan <zzz2999582@gmail.com>
2021-05-15 17:00:16 +0800

    fix: vue.config to use uglifyjs

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,17 @@ const isProd = process.env.NODE_ENV === "production"
 
 module.exports = {
   publicPath: isProd ? './' : './',
-  optimization: {
-    minimizer: isProd ? [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            drop_console: true
-          },
-        }
-      })
-    ] : []
+  configureWebpack: {
+    optimization: {
+      minimizer: isProd ? [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              drop_console: true
+            },
+          }
+        })
+      ] : []
+    }
   }
 }
